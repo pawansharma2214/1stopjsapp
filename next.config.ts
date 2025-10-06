026b1b680fb44cfe6728e6e12d8adef34d74c526
sandeep-barala <sandeep.barala@teaminindia.com>
2025-10-06 16:50:10 +0530

    update config js file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // optional but recommended  
+  /* config options here */
 };
 
 export default nextConfig;
