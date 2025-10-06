f5b54f421c3532482b5d964fb7de37d2c6586f9d
sandeep-barala <sandeep.barala@teaminindia.com>
2025-10-06 16:32:16 +0530

    add about and product pages with app router

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // must be true for App Router
+  },
 };
 
 export default nextConfig;
