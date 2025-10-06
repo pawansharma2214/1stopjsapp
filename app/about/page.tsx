94690580bcdd6b6d6997460c2e6d3fcd7471bd03
sandeep-barala <sandeep.barala@teaminindia.com>
2025-10-06 16:26:26 +0530

    two pages added about and product

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>Welcome to the About page of our Next.js app!</p>
+    </main>
+  );
+}
---
+++ app/project/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductPage() {
+  return (
+    <main>
+      <h1>Our Products</h1>
+      <p>Here you’ll find all our amazing products.</p>
+    </main>
+  );
+}
