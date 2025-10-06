139e0339a334636e60514d7cdb71add749103d60
sandeep-barala <sandeep.barala@teaminindia.com>
2025-10-06 16:46:52 +0530

    add about and product pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // must be true for App Router
-  },
+  reactStrictMode: true, // optional but recommended  
 };
 
 export default nextConfig;
